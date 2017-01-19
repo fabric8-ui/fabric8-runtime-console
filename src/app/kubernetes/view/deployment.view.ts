7d9bb29914fe1ad86e4c1d0c496680c368964538
James Strachan <james.strachan@gmail.com>
2017-01-19 06:52:30 +0000

    add nicer view of deployments

--- src/app/kubernetes/view/deployment.view.ts
+++ src/app/kubernetes/view/deployment.view.ts
@@ -1,22 +1,74 @@
 import {Service, Services} from "../../kubernetes/model/kuberentes.service.model";
 import {Deployment, Deployments} from "../model/kuberentes.deployment.model";
 
+export class Entry {
+  constructor(public key: string, public value: string) {}
+}
+
+/**
+ * Creates an array of entries from the given map
+ */
+export function createEntryArray(labels: Map<string,string>): Array<Entry> {
+  var entries = new Array<Entry>();
+  if (labels) {
+    Object.keys(labels).forEach((k) => entries.push(new Entry(k, labels[k])));
+  }
+  return entries;
+}
+
 export class DeploymentView {
     public readonly deployment: Deployment;
     public readonly service: Service;
     public readonly id: string;
     public readonly name: string;
+    public readonly icon: string;
     public readonly description: string;
     public readonly exposeUrl: string;
+    public readonly replicas: number;
+    public readonly availableReplicas: number;
+    public readonly labels: Map<string,string>;
+    public readonly labelEntries: Array<Entry>;
+    public readonly images: Array<String>;
+    public readonly annotations: Map<string,string>;
 
     constructor(deployment: Deployment, service: Service) {
         this.deployment = deployment;
         this.service = service;
         this.id = deployment.id;
         this.name = deployment.name;
+        this.icon = deployment.icon;
         this.description = deployment.description;
+        this.labels = deployment.labels;
+        this.labelEntries = createEntryArray(this.labels);
+        this.annotations = deployment.annotations;
         if (service) {
             this.exposeUrl = service.exposeUrl;
+        }
+        this.images = new  Array<String>();
+        var spec = deployment.spec;
+        if (spec) {
+          var template = spec.template;
+          if (template) {
+            var podSpec = template.spec;
+            if (podSpec) {
+              var containers = podSpec.containers;
+              if (containers) {
+                containers.forEach((c) => {
+                  var image = c.image;
+                  if (image) {
+                    this.images.push(image);
+                  }
+                });
+              }
+            }
+          }
+        }
+        this.replicas = 0;
+        this.availableReplicas = 0;
+        var status = deployment.status;
+        if (status) {
+            this.replicas = status.replicas;
+            this.availableReplicas = status.availableReplicas;
         }
     }
 }
