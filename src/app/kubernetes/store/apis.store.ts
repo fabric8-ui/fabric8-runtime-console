04bb037099d5a04dbddeaedf0ded619c3eb9dc0a
James Strachan <james.strachan@gmail.com>
2017-01-24 15:15:12 +0000

    lets optimise away unnecessary swagger calls

--- src/app/kubernetes/store/apis.store.ts
+++ src/app/kubernetes/store/apis.store.ts
@@ -3,11 +3,15 @@ import {BehaviorSubject, Observable} from "rxjs";
 import {APIs} from "../model/apis.model";
 import {Http} from "@angular/http";
 
+/**
+ * Lets keep around the singleton results to avoid doing too many requests for this static data
+ */
+var latestAPIs: APIs = null;
+
 @Injectable()
 export class APIsStore {
   private _current: BehaviorSubject<APIs> = new BehaviorSubject(new APIs());
   private _loading: BehaviorSubject<boolean> = new BehaviorSubject(false);
-  private _latest: APIs = null;
 
   constructor(private http: Http) {
     this.load();
@@ -21,10 +25,6 @@ export class APIsStore {
     return this._loading.asObservable();
   }
 
-  get latest(): APIs {
-    return this._latest;
-  }
-
   /**
    * Returns whether we are running against openshift.
    *
@@ -33,7 +33,7 @@ export class APIsStore {
    * @return {boolean} true if this cluster is using openshift
    */
   isOpenShift(): boolean {
-    let apis = this.latest;
+    let apis = latestAPIs;
     if (!apis) {
       console.log("WARNING: invoked the isOpenShift() method before the APIsStore has loaded!");
       return true;
@@ -42,23 +42,26 @@ export class APIsStore {
   }
 
   load() {
-    this._loading.next(true);
-    this.http.get("/swaggerapi")
-      .map(res => {
-        var body = res.json() || {};
-        return new APIs(body.apis || []);
-      })
-      .subscribe(
-        (apis) => {
-          this._latest = apis;
-          this._current.next(apis);
-          this._loading.next(false);
-        },
-        (error) => {
-          console.log('Error retrieving APIs: ' + error);
-          this._current.error(error);
-          this._loading.error(error);
+    // we only need to load once really on startup
+    if (!latestAPIs) {
+      this._loading.next(true);
+      this.http.get("/swaggerapi")
+        .map(res => {
+          var body = res.json() || {};
+          return new APIs(body.apis || []);
+        })
+        .subscribe(
+          (apis) => {
+            latestAPIs = apis;
+            this._current.next(apis);
+            this._loading.next(false);
+          },
+          (error) => {
+            console.log('Error retrieving APIs: ' + error);
+            this._current.error(error);
+            this._loading.error(error);
 
-        });
+          });
+    }
   }
 }
