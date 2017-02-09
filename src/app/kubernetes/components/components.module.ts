541047c36c4d03acbd78d63956fedb4e0149227e
James Strachan <james.strachan@gmail.com>
2017-02-09 22:14:28 +0000

    fixed build

--- src/app/kubernetes/components/components.module.ts
+++ src/app/kubernetes/components/components.module.ts
@@ -3,12 +3,10 @@ import {PodPhaseIconComponent} from "./pod-phase-icon/pod-phase-icon";
 import {KubernetesLabelsComponent} from "./k8s-labels/k8s-labels";
 import {Fabric8CommonModule} from "../../common/common.module";
 import {CommonModule} from "@angular/common";
-import {BrowserModule} from "@angular/platform-browser";
 
 @NgModule({
   imports: [
     CommonModule,
-    BrowserModule,
     Fabric8CommonModule,
   ],
   declarations: [
