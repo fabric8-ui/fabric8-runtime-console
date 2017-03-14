803fd05d5d79b81eaf1a80266ee271096422eb2b
Dana Gutride <dgutride@redhat.com>
2017-03-13 22:44:47 -0400

    fix(export): add missing dependencies

--- index.ts
+++ index.ts
@@ -1,1 +1,4 @@
 export { KubernetesStoreModule } from './src/app/kubernetes/kubernetes.store.module';
+
+export { SpaceModule } from './src/app/kubernetes/ui/space/space.module';
+export { SpacesListComponent } from './src/app/kubernetes/ui/space/list/list.space.component';
