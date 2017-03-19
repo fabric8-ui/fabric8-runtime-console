0dfb6c91f5d51d2d830921689b0943648f5bf985
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-19 10:43:42 -0400

    fix(pipeline): export the pipeline component

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 export { KubernetesStoreModule } from './src/app/kubernetes/kubernetes.store.module';
-
 export { SpaceModule } from './src/app/kubernetes/ui/space/space.module';
+export { PipelineModule } from './src/app/kubernetes/ui/pipeline/pipeline.module';
 export { SpacesListComponent } from './src/app/kubernetes/ui/space/list/list.space.component';
 export { KubernetesRestangularModule } from './src/app/kubernetes/service/kubernetes.restangular';
