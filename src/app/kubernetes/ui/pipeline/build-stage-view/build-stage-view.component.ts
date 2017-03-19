ee7c6fa01efedc87c59ef16ec0fc753043539a64
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-19 10:43:59 -0400

    fix(pipelines): fix URLs for scss and html

--- src/app/kubernetes/ui/pipeline/build-stage-view/build-stage-view.component.ts
+++ src/app/kubernetes/ui/pipeline/build-stage-view/build-stage-view.component.ts
@@ -3,8 +3,8 @@ import {Build} from "../../../model/build.model";
 
 @Component({
   selector: 'build-stage-view',
-  templateUrl: 'build-stage-view.component.html',
-  styleUrls: ['build-stage-view.component.scss'],
+  templateUrl: './build-stage-view.component.html',
+  styleUrls: ['./build-stage-view.component.scss'],
 })
 export class BuildStageViewComponent {
 
