34ca38f1e678c1b2319ba616b4f04478bbebdc75
rawlingsj <rawlingsj80@gmail.com>
2017-03-21 18:35:56 +0000

    fix(release): add CI pipeline stage to build and deploy PR changes into fabric8-ui and deploy it to test cluster

--- release.groovy
+++ release.groovy
@@ -1,13 +1,45 @@
 #!/usr/bin/groovy
 
 def ci (){
-    stage('install'){
+
+    stage('build runtime npm'){
         sh 'npm install'
         sh 'npm run build'
     }
 
     stage('test'){
         sh 'npm test'
+    }
+}
+
+def ciBuildDownstreamProject(){
+    def runtimeDir = pwd()
+
+    stage('build fabric8-ui npm'){
+        sh 'git clone https://github.com/fabric8io/fabric8-ui'
+        sh 'cd fabric8-ui && npm install'
+        sh "cd fabric8-ui && npm install --save  ${runtimeDir}/dist"
+        sh '''
+        export FABRIC8_WIT_API_URL="http://api.prod-preview.openshift.io/api/"
+        export FABRIC8_RECOMMENDER_API_URL="http://api-bayesian.dev.rdu2c.fabric8.io/api/v1/"
+        export FABRIC8_FORGE_API_URL="https://forge.api.prod-preview.openshift.io"
+        export FABRIC8_SSO_API_URL="http://sso.prod-preview.openshift.io/"
+
+        cd fabric8-ui && npm run build:prod
+        '''
+    }
+    def shortCommitSha = getNewVersion {}
+    def tempVersion= 'SNAPSHOT.' + shortCommitSha + env.BUILD_NUMBER
+    return tempVersion
+}
+
+def buildImage(imageName){
+    stage('build snapshot image'){
+        sh "cd fabric8-ui && docker build -t ${imageName} -f Dockerfile.deploy ."
+    }        
+    
+    stage('push snapshot image'){
+        sh "cd fabric8-ui && docker push ${imageName}"
     }
 }
 
