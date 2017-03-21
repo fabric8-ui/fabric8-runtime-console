d6b5fb8ed6e8061f4bea15502fb8572481c51722
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-21 14:09:24 -0400

    fix(library): export a few more classes

--- index.ts
+++ index.ts
@@ -3,3 +3,16 @@ export { SpaceModule } from './src/app/kubernetes/ui/space/space.module';
 export { PipelineModule } from './src/app/kubernetes/ui/pipeline/pipeline.module';
 export { SpacesListComponent } from './src/app/kubernetes/ui/space/list/list.space.component';
 export { KubernetesRestangularModule } from './src/app/kubernetes/service/kubernetes.restangular';
+export {
+  BuildConfig,
+  BuildConfigs,
+  combineBuildConfigAndBuilds,
+  filterPipelines
+} from './src/app/kubernetes/model/buildconfig.model';
+export { BuildConfigStore } from './src/app/kubernetes/store/buildconfig.store';
+export { BuildStore } from './src/app/kubernetes/store/build.store';
+export { OnLogin } from './src/app/shared/onlogin.service';
+export { OAuthConfigStore } from './src/app/kubernetes/store/oauth-config-store';
+export { APIsStore } from './src/app/kubernetes/store/apis.store';
+export { LoginService } from './src/app/shared/login.service';
+export { NamespaceScope } from './src/app/kubernetes/service/namespace.scope';
