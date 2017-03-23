11d09a943ac13f94755e2bd9c8e02dddb6205d99
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-23 09:56:06 -0400

    fix: export models and services

--- index.ts
+++ index.ts
@@ -1,3 +1,26 @@
+export { PipelineStatusComponent } from './src/app/kubernetes/components/pipeline-status/pipeline-status.component';
+export { PodPhaseIconComponent } from './src/app/kubernetes/components/pod-phase-icon/pod-phase-icon.component';
+export { KubernetesLabelsComponent } from './src/app/kubernetes/components/k8s-labels/k8s-labels.component';
+export { BuildStatusIconComponent } from './src/app/kubernetes/components/build-status-icon/build-status-icon.component';
+export { KubernetesComponentsModule } from './src/app/kubernetes/components/components.module';
+export { Space, Environment, Spaces, asSpaces } from './src/app/kubernetes/model/space.model';
+export { Service, Services, enrichServiceWithRoute } from './src/app/kubernetes/model/service.model';
+export { ScalableResource } from './src/app/kubernetes/model/scalableresource.model';
+export { Routes } from '@angular/router';
+export { Route } from './src/app/kubernetes/model/route.model';
+export { ReplicationController, ReplicationControllers } from './src/app/kubernetes/model/replicationcontroller.model';
+export { ReplicaSet, ReplicaSets } from './src/app/kubernetes/model/replicaset.model';
+export { Pod, Pods } from './src/app/kubernetes/model/pod.model';
+export { PipelineStage } from './src/app/kubernetes/model/pipelinestage.model';
+export { Namespace, Namespaces, isSecretsNamespace, isSystemNamespace } from './src/app/kubernetes/model/namespace.model';
+export { KubernetesResource } from './src/app/kubernetes/model/kubernetesresource.model';
+export { KubernetesSpecResource } from './src/app/kubernetes/model/kuberentesspecresource.model';
+export { resourceKindToCollectionName, isNamespacedKind } from './src/app/kubernetes/model/helpers';
+export { Events } from './src/app/kubernetes/model/event.model';
+export { DeploymentConfig, DeploymentConfigs } from './src/app/kubernetes/model/deploymentconfig.model';
+export { Deployment, Deployments } from './src/app/kubernetes/model/deployment.model';
+export { ConfigMap, ConfigMaps } from './src/app/kubernetes/model/configmap.model';
+export { Build, Builds } from './src/app/kubernetes/model/build.model';
 export { KubernetesStoreModule } from './src/app/kubernetes/kubernetes.store.module';
 export { SpaceModule } from './src/app/kubernetes/ui/space/space.module';
 export { PipelineModule } from './src/app/kubernetes/ui/pipeline/pipeline.module';
@@ -7,7 +30,8 @@ export {
   BuildConfig,
   BuildConfigs,
   combineBuildConfigAndBuilds,
-  filterPipelines
+  filterPipelines,
+  findBuildConfigByID
 } from './src/app/kubernetes/model/buildconfig.model';
 export { BuildConfigStore } from './src/app/kubernetes/store/buildconfig.store';
 export { BuildStore } from './src/app/kubernetes/store/build.store';
@@ -17,3 +41,31 @@ export { APIsStore } from './src/app/kubernetes/store/apis.store';
 export { LoginService } from './src/app/shared/login.service';
 export { NamespaceScope } from './src/app/kubernetes/service/namespace.scope';
 export { DevNamespaceScope } from './src/app/kubernetes/service/devnamespace.scope';
+export { DeploymentModule } from './src/app/kubernetes/ui/deployment/deployment.module';
+export { createDeploymentViews } from './src/app/kubernetes/view/deployment.view';
+export { CompositeDeploymentStore } from './src/app/kubernetes/store/compositedeployment.store';
+export { ServiceStore } from './src/app/kubernetes/store/service.store';
+export { ServiceService } from './src/app/kubernetes/service/service.service';
+export { DeploymentService } from './src/app/kubernetes/service/deployment.service';
+export { DeploymentStore } from './src/app/kubernetes/store/deployment.store';
+export { EventService } from './src/app/kubernetes/service/event.service';
+export { EventStore } from './src/app/kubernetes/store/event.store';
+export { PodService } from './src/app/kubernetes/service/pod.service';
+export { PodStore } from './src/app/kubernetes/store/pod.store';
+export { NamespaceService } from './src/app/kubernetes/service/namespace.service';
+export { NamespaceStore } from './src/app/kubernetes/store/namespace.store';
+export { ReplicaSetService } from './src/app/kubernetes/service/replicaset.service';
+export { ReplicaSetStore } from './src/app/kubernetes/store/replicaset.store';
+export { ReplicationControllerService } from './src/app/kubernetes/service/replicationcontroller.service';
+export { ReplicationControllerStore } from './src/app/kubernetes/store/replicationcontroller.store';
+export { ConfigMapService } from './src/app/kubernetes/service/configmap.service';
+export { ConfigMapStore } from './src/app/kubernetes/store/configmap.store';
+export { BuildConfigService } from './src/app/kubernetes/service/buildconfig.service';
+export { DeploymentConfigService } from './src/app/kubernetes/service/deploymentconfig.service';
+export { DeploymentConfigStore } from './src/app/kubernetes/store/deploymentconfig.store';
+export { BuildService } from './src/app/kubernetes/service/build.service';
+export { SpaceStore } from './src/app/kubernetes/store/space.store';
+export { OAuthService } from 'angular2-oauth2/oauth-service';
+export { RouteServiceStore } from './src/app/kubernetes/store/route.service.store';
+export { RouteService } from './src/app/kubernetes/service/route.service';
+export { RouteStore } from './src/app/kubernetes/store/route.store';
