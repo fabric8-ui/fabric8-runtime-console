49bcf70c15fb78386a64dff797d1bf1732d2bc0a
James Strachan <james.strachan@gmail.com>
2017-04-28 10:41:41 +0100

    feat(apps page): add kebab menu
    lets add a kebab menu to drop down so folks can scale, delete or view on openshift console. Also lets remove the icon on the versions and just use links to make things look easier to grok #454

--- src/app/kubernetes/ui/app/list/list.app.component.ts
+++ src/app/kubernetes/ui/app/list/list.app.component.ts
@@ -1,8 +1,10 @@
-import {Component, Input} from "@angular/core";
+import {Component, Input, ViewChild} from "@angular/core";
 import {TREE_ACTIONS} from "angular2-tree-component";
 import {ParentLinkFactory} from "../../../../common/parent-link-factory";
 import {AppDeployments} from "../list-page/list-page.app.component";
 import {Space, createEmptySpace} from "../../../model/space.model";
+import {DeploymentDeleteDialog} from "../../deployment/delete-dialog/delete-dialog.deployment.component";
+import {DeploymentScaleDialog} from "../../deployment/scale-dialog/scale-dialog.deployment.component";
 
 @Component({
   selector: 'fabric8-apps-list',
@@ -16,10 +18,26 @@ export class AppListComponent {
   @Input() apps: AppDeployments[];
   @Input() space: Space;
 
+
+  @ViewChild(DeploymentDeleteDialog) deleteDialog: DeploymentDeleteDialog;
+  @ViewChild(DeploymentScaleDialog) scaleDialog: DeploymentScaleDialog;
+
   constructor(
     parentLinkFactory: ParentLinkFactory,
   ) {
     this.parentLink = parentLinkFactory.parentLink;
   }
+
+  openDeleteDialog(deleteDeploymentModal, deployment) {
+    this.deleteDialog.modal = deleteDeploymentModal;
+    this.deleteDialog.deployment = deployment;
+    deleteDeploymentModal.open();
+  }
+
+  openScaleDialog(scaleDeploymentModal, deployment) {
+    this.scaleDialog.configure(scaleDeploymentModal, deployment);
+    scaleDeploymentModal.open();
+  }
+
 
 }
